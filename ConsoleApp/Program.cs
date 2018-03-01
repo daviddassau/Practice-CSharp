825b46c90b6f2356491d5f82128e0e01df833381
David Dassau <todieahero@hotmail.com>
2018-03-01 13:38:02 -0600

    dictionary 5

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -22,7 +22,15 @@ namespace ConsoleApp
 
             foreach (var key in keys)
             {
-                Console.WriteLine($"{key}: {inventory[key]}");
+                Console.WriteLine($"{key}: {inventory[key]}"); // this will display the items in the dictionary as they're listed in the dictionary
+            }
+
+            string[] keysArray = keys.ToArray();
+            Array.Sort(keysArray);
+
+            foreach (var key in keysArray)
+            {
+                Console.WriteLine($"{key}: {inventory[key]}"); // this will display the items in the dictionary in alphabetical order
             }
 
             Console.ReadLine();
