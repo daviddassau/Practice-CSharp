b513d7a653531df80dc3558f3d0aae3107ffdf9e
David Dassau <todieahero@hotmail.com>
2018-02-28 22:00:56 -0600

    lists 2

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -21,7 +21,25 @@ namespace ConsoleApp
                 Console.WriteLine(item);
             }
 
+            // Sort will sort the items in alphabetical order
+            fruitList.Sort();
+            foreach (var item in fruitList)
+            {
+                Console.WriteLine(item);
+            }
+
+            ReportOnGrapes(fruitList);
+
             Console.ReadLine();
+        }
+
+        static void ReportOnGrapes(List<string> items)
+        {
+            string grapeReport = 
+                items.Contains("Grapes", StringComparer.OrdinalIgnoreCase) ? 
+                "Yes there are grapes" :
+                "No, there are no grapes";
+            Console.WriteLine(grapeReport);
         }
     }
 }
