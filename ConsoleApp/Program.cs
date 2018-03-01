0e9d929210eaebb0f2e59fe34331f21b17023ac2
David Dassau <todieahero@hotmail.com>
2018-03-01 14:01:17 -0600

    representing data with plain old class objects 1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,71 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace SimpleCalculator
+namespace ConsoleApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //Get values to calculate
-            double double1 = GetValue("Enter value 1: ");
-            double double2 = GetValue("Enter value 2: ");
+            var produce = new List<object>();
 
-            //Declare variable to hold result
-            double result = 0;
 
-            //Do math operation
-            while (true)
-            {
-                Console.Write("(A)dd (S)ubtract (M)ultiply (D)ivide : ");
+            var f1 = new Fruit();
+            produce.Add(f1);
 
-                ConsoleKeyInfo info = Console.ReadKey();
-                string operation = info.Key.ToString();
+            var f2 = new Fruit();
+            produce.Add(f2);
 
-                switch (operation.ToUpper())
-                {
-                    case "A":
-                        result = CalcUtilities.Add(double1, double2);
-                        break;
-                    case "S":
-                        result = CalcUtilities.Subtract(double1, double2);
-                        break;
-                    case "M":
-                        result = CalcUtilities.Multiply(double1, double2);
-                        break;
-                    case "D":
-                        result = CalcUtilities.Divide(double1, double2);
-                        break;
-                    default:
-                        Console.WriteLine("Choose from supported operations");
-                        continue;
-                }
-                Console.WriteLine("\nResult: " + result);
-                Console.Read();
-                break;
-            }
+            
+            produce.Add(new Vegetable());
 
+            Console.WriteLine($"There are {produce.Count} items");
+
+
+
+            Console.ReadLine();
         }
+    }
 
-        private static double GetValue(string label)
-        {
-            //the value to be returned
-            double value;
+    class Fruit
+    {
 
-            //loop until you get a valid entry
-            while (true)
-            {
-                Console.Write(label);
-                string input = Console.ReadLine();
-                if (Double.TryParse(input, out value))
-                {
-                    return value;
-                }
-                else
-                {
-                    Console.WriteLine("Value can't be parsed as a number");
-                }
-            }
-        }
+    }
+
+    class Vegetable
+    {
 
     }
 }
