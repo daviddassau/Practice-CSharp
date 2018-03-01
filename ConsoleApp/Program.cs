34c6d5b675d4c34172027b98c8e4d2018a8758b1
David Dassau <todieahero@hotmail.com>
2018-02-28 20:27:43 -0600

    scope 1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,9 +8,6 @@ namespace ConsoleApp
 {
     class Program
     {
-        const int Value1 = 12;
-        const int Value2 = 24;
-
         static void Main(string[] args)
         {
             //string input = Console.ReadLine();
@@ -112,24 +109,12 @@ namespace ConsoleApp
 
 
 
-            int total = Add(Value1, Value2);
-            Console.WriteLine("Total: " + total);
-
-            int result;
-            AddToOutParam(Value1, Value2, out result);
-            Console.WriteLine("The answer is: " + result);
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Value of i: " + i);
+            }
 
             Console.ReadLine();
-        }
-
-        static int Add(int value1, int value2)
-        {
-            return value1 + value2;
-        }
-
-        static void AddToOutParam(int value1, int value2, out int result)
-        {
-            result = value1 + value2;
         }
     }
 }
