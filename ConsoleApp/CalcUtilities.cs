4b9240a336c09132f873225203aea3c24b86869d
David Dassau <todieahero@hotmail.com>
2018-03-01 13:53:30 -0600

    defining class with static methods 2

--- ConsoleApp/CalcUtilities.cs
+++ ConsoleApp/CalcUtilities.cs
@@ -4,9 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp
+namespace SimpleCalculator
 {
     class CalcUtilities
     {
+        public static double Add(double double1, double double2)
+        {
+            return double1 + double2;
+        }
+
+        public static double Subtract(double double1, double double2)
+        {
+            return double1 - double2;
+        }
+
+        public static double Multiply(double double1, double double2)
+        {
+            return double1 * double2;
+        }
+
+        public static double Divide(double double1, double double2)
+        {
+            if (double1 == 0 || double2 == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return double1 / double2;
+            }
+        }
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,50 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp
+namespace SimpleCalculator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var inventory = new Dictionary<string, double>();
+            //Get values to calculate
+            double double1 = GetValue("Enter value 1: ");
+            double double2 = GetValue("Enter value 2: ");
 
-            inventory.Add("Figs", 56);
-            inventory.Add("Apples", 23);
-            inventory.Add("Oranges", 12);
+            //Declare variable to hold result
+            double result = 0;
 
-            var keys = inventory.Keys;
-
-            Console.WriteLine($"Number of items: {keys.Count}");
-
-            foreach (var key in keys)
+            //Do math operation
+            while (true)
             {
-                Console.WriteLine($"{key}: {inventory[key]}"); // this will display the items in the dictionary as they're listed in the dictionary
+                Console.Write("(A)dd (S)ubtract (M)ultiply (D)ivide : ");
+
+                ConsoleKeyInfo info = Console.ReadKey();
+                string operation = info.Key.ToString();
+
+                switch (operation.ToUpper())
+                {
+                    case "A":
+                        result = CalcUtilities.Add(double1, double2);
+                        break;
+                    case "S":
+                        result = CalcUtilities.Subtract(double1, double2);
+                        break;
+                    case "M":
+                        result = CalcUtilities.Multiply(double1, double2);
+                        break;
+                    case "D":
+                        result = CalcUtilities.Divide(double1, double2);
+                        break;
+                    default:
+                        Console.WriteLine("Choose from supported operations");
+                        continue;
+                }
+                Console.WriteLine("\nResult: " + result);
+                Console.Read();
+                break;
             }
 
-            string[] keysArray = keys.ToArray();
-            Array.Sort(keysArray);
-
-            foreach (var key in keysArray)
-            {
-                Console.WriteLine($"{key}: {inventory[key]}"); // this will display the items in the dictionary in alphabetical order
-            }
-
-            // this will test what's passed in ("Figs") and see if it's true or false
-            double value;
-            if (inventory.TryGetValue("Figs", out value))
-            {
-                Console.WriteLine($"Figs inventory: {value}");
-            }
-            else
-            {
-                Console.WriteLine("Figs inventory not available");
-            }
-
-            Console.ReadLine();
         }
+
+        private static double GetValue(string label)
+        {
+            //the value to be returned
+            double value;
+
+            //loop until you get a valid entry
+            while (true)
+            {
+                Console.Write(label);
+                string input = Console.ReadLine();
+                if (Double.TryParse(input, out value))
+                {
+                    return value;
+                }
+                else
+                {
+                    Console.WriteLine("Value can't be parsed as a number");
+                }
+            }
+        }
+
     }
 }
