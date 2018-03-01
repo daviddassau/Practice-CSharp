6b5be974c8a908958e6644350419c489d4920953
David Dassau <todieahero@hotmail.com>
2018-02-28 21:10:43 -0600

    finished chapter 5

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,47 +8,45 @@ namespace ConsoleApp
 {
     class Program
     {
-        enum Fruit { Apple, Orange, Fig };
-
         static void Main(string[] args)
         {
-            var thisFruit = Fruit.Apple;
-            switch (thisFruit)
-            {
-                case Fruit.Apple:
-                    Console.WriteLine("You chose Apple");
-                    break;
-                case Fruit.Orange:
-                    Console.WriteLine("You chose Orange");
-                    break;
-                case Fruit.Fig:
-                    Console.WriteLine("You chose Fig");
-                    break;
-                default:
-                    Console.WriteLine("You chose something else");
-                    break;
-            }
+            //var thisFruit = Fruit.Apple;
+            //switch (thisFruit)
+            //{
+            //    case Fruit.Apple:
+            //        Console.WriteLine("You chose Apple");
+            //        break;
+            //    case Fruit.Orange:
+            //        Console.WriteLine("You chose Orange");
+            //        break;
+            //    case Fruit.Fig:
+            //        Console.WriteLine("You chose Fig");
+            //        break;
+            //    default:
+            //        Console.WriteLine("You chose something else");
+            //        break;
+            //}
 
-            Fruit[] groceries = { Fruit.Apple, Fruit.Apple, Fruit.Orange };
+            //Fruit[] groceries = { Fruit.Apple, Fruit.Apple, Fruit.Orange };
 
-            int apples = 0;
-            foreach (var item in groceries)
-            {
-                if (item == Fruit.Orange)
-                {
-                    break;
-                }
-                if (item == Fruit.Apple)
-                {
-                    apples++;
-                    if (apples > 1)
-                    {
-                        continue;
-                    }
-                }
-                Console.WriteLine(item);
-            }
-            Console.WriteLine("End of grocery list");
+            //int apples = 0;
+            //foreach (var item in groceries)
+            //{
+            //    if (item == Fruit.Orange)
+            //    {
+            //        break;
+            //    }
+            //    if (item == Fruit.Apple)
+            //    {
+            //        apples++;
+            //        if (apples > 1)
+            //        {
+            //            continue;
+            //        }
+            //    }
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("End of grocery list");
 
 
 
