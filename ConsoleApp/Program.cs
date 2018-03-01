29bc9df11090c6d627bfacdb5ef694f26a4e7642
David Dassau <todieahero@hotmail.com>
2018-02-28 21:51:19 -0600

    simple arrays 6

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,37 +10,33 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            string[] fruit = { "Apples", "Oranges", "Grapes" };
-            foreach (var item in fruit)
+            string[,] sections = new string[3, 3];
+
+            sections[0, 0] = "top left";
+            sections[0, 1] = "top center";
+            sections[0, 2] = "top right";
+
+            sections[1, 0] = "middle left";
+            sections[1, 1] = "middle center";
+            sections[1, 2] = "middle right";
+
+            sections[2, 0] = "bottom left";
+            sections[2, 1] = "bottom center";
+            sections[2, 2] = "bottom right";
+
+            for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine(item);
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(sections[i,j] + "\t");
+                    if ((j+1) % 3 == 0)
+                    {
+                        Console.WriteLine("");
+                    }
+                }
             }
-
-            string[] names = new string[3];
-            names[0] = "Joe";
-            names[1] = "Mary";
-            names[2] = "Martha";
-
-            int[] weights = {12, 34, 56, 12, 23};
-            int sum = weights.Sum();
-            Console.WriteLine($"The sum is {sum}");
-
-            double average = weights.Average();
-            Console.WriteLine($"The average is {average}");
-
-            UseArray(names);
-
 
             Console.ReadLine();
-        }
-
-        static void UseArray(string[] values)
-        {
-            Console.WriteLine("Values in method: ");
-            foreach (var item in values)
-            {
-                Console.WriteLine(item);
-            }
         }
     }
 }
