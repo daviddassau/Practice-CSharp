1840f420cc108aee912e126dfac249d764682540
David Dassau <todieahero@hotmail.com>
2018-03-02 14:15:36 -0600

    methods that accept subclassed objects 1

---
+++ ConsoleApp/Model/ProduceUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Model
+{
+    class ProduceUtility
+    {
+        public static double GetItemWeight(Produce item)
+        {
+            return item.Weight + item.Quantity;
+        }
+    }
+}
